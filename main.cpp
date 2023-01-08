a0db9270314bd8c34a7e9e3108ee8311ffe8a38a
Safal <sawfal24@gmail.com>
2023-01-08 20:53:30 +0545

    Added readme and required files

--- main.cpp
+++ main.cpp
@@ -1,56 +1,68 @@
-#include<stdio.h>
+/*
+basic things:
+if moving from left to right, increase x by 1 else by -1.
+if moving from up to down, increase y by 1 else by -1
+
+if slope < 1, p = 2dy - dx
+    if p < 0, increase x, next p = p + 2dy
+    else, increase x, increase y, next p = p + 2dy - 2dx
+
+if slope > 1, replace dy with dx and vice versa
+*/
+
 #include<graphics.h>
+#include<stdio.h>
+#include<math.h>
 
-void lineDDA(int x1, int y1, int x2, int y2){
-    int dx = x2 - x1;
-    int dy = y2 - y1;
-    int steps;
-    float xincrement, yincrement, x = x1, y = y1;
+void bresenline(int x1, int y1, int x2, int y2){
+    int x = x1, y = y1;
+    int dx, dy, xinc, yinc, p;
+    dx = abs(x2 - x1);
+    dy = abs(y2 - y1);
 
-    if(abs(dx) > abs(dy))
-    {
-        steps = abs(dx);
-    } 
-    else 
-    {
-        steps = abs(dy);
+    xinc = (x2>x1) ? 1 : -1;
+    yinc = (y2>y1)? 1 : -1;
+
+    if(dx>dy){ //slope < 1
+        p = 2*dy - dx;
+
+        for(int k = 0; k<dx; k++){
+            x += xinc; //x is always increased if slope < 1
+            if(p<0){
+                p += 2*dy;
+            }
+            else{
+                y += yinc;
+                p += 2*dy - 2*dx;
+            }
+            putpixel(x, y, WHITE);
+        }
+    } //else, slope > 1
+    else{
+        p = 2*dx - dy;
+
+        for(int k = 0; k<dy; k++){
+            y += yinc; //y is always increased if slope > 0
+            if(p<0){
+                p += 2*dx;
+            }
+            else{
+                x += xinc;
+                p += 2*dx - 2*dy;
+            }
+            putpixel(x,y, WHITE);
+        }
     }
-
-    //increase xk or yk by slope or 1/slope or 1
-    xincrement = dx/(float)steps;
-    yincrement = dy/(float)steps;
-
-    for(int i = 0; i<steps; i++){
-        x += xincrement;
-        y += yincrement;
-        putpixel(x, y, WHITE);
-        // delay(1);
-    }
-
-}
-
-void createman(){
-    circle(400, 100, 80);
-    //body
-    line(400, 180, 400, 400);
-    //legs
-    line(400, 400, 350, 590);
-    line(400, 400, 450, 590);
-    //hands
-    line(400, 200, 300, 350);
-    line(400, 200, 500, 350);
-    //eyes
-    ellipse(360, 75, 0, 360, 15, 12);
-    ellipse(440, 75, 0, 360, 15, 12);
-    //mouth
-    arc(400, 120, 200, 340, 40);
 }
 
 int main(){
-    initwindow(800, 600, "DDA");
-    // createman();
-    // lineDDA(400, 400, 200, 200);
-    lineDDA(0,0, 800, 600);
+    initwindow(800, 600, "Safal Karki's implementation of Bresenham's Line Algorithm");
+    bresenline(0, 300, 800, 300);
+    bresenline(400, 0, 400, 600);
+    bresenline(0, 300, 400, 0);
+    bresenline(400, 0, 800, 300);
+    bresenline(800, 300, 400, 600);
+    bresenline(400, 600, 0, 300);
     getch();
     closegraph();
     return 0;
---
+++ readme.md
@@ -0,0 +1,18 @@
+#Dumping Computer Graphics Lab works
+
+To setup graphics.h:
+From bgi_files folder,
+->Copy .a file to lib folder of compiler
+->Copy .h file to include folder of compiler
+
+To compile:
+By linking the following libraries while compiling:
+-lbgi,
+-lgdi32
+-lcomdlg32
+-luuid
+-loleaut32
+-lole32
+
+OR, by editing the bat file:
+rename the .cpp filename in run.bat to the file to be compiled and execute the bat file from terminal.
