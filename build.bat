3b1b4faa429fbc85294e0ec2c18ccc85050f7f17
Safal <sawfal24@gmail.com>
2023-01-09 18:03:56 +0545

    now displays message after building, without inverted commas

--- build.bat
+++ build.bat
@@ -2,5 +2,5 @@
 mkdir bin
 pushd bin
 g++ -fdiagnostics-color=always -g ../main.cpp -o main.exe -lbgi -lgdi32 -lcomdlg32 -luuid -loleaut32 -lole32
-echo "Executable created in bin"
+echo Executable created in bin
 popd
