159de8fea320a7e5aecfa05a8bf3abae8eeddd39
Safal <sawfal24@gmail.com>
2023-01-09 18:33:02 +0545

    added if else statements

--- build.bat
+++ build.bat
@@ -1,6 +1,14 @@
 @echo off
 mkdir bin
 pushd bin
+
+rem %cd% is current directory
+rem set foo=%cd%, now foo is %cd%
 g++ -fdiagnostics-color=always -g ../%1 -o main.exe -lbgi -lgdi32 -lcomdlg32 -luuid -loleaut32 -lole32
-echo Executable created in bin
+
+if exist main.exe (
+    echo Executable created in bin
+) else (
+    echo Couldn't compile
+)
 popd
