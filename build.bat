19aa1ff15fe4d0aacfbace520d4f516a9c2a7216
Safal <sawfal24@gmail.com>
2023-01-09 18:34:40 +0545

    added comment about %1

--- build.bat
+++ build.bat
@@ -4,6 +4,8 @@ pushd bin
 
 rem %cd% is current directory
 rem set foo=%cd%, now foo is %cd%
+
+rem %1 is command line argument. %1 to %9 can be used
 g++ -fdiagnostics-color=always -g ../%1 -o main.exe -lbgi -lgdi32 -lcomdlg32 -luuid -loleaut32 -lole32
 
 if exist main.exe (
