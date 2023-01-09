7d9eae710281aea45870aa0ccf19fdf9ca5b8a3e
Safal <sawfal24@gmail.com>
2023-01-09 18:47:03 +0545

    moved to src folder

--- build.bat
+++ build.bat
@@ -6,7 +6,7 @@ rem %cd% is current directory
 rem set foo=%cd%, now foo is %cd%
 
 rem %1 is command line argument. %1 to %9 can be used
-g++ -fdiagnostics-color=always -g ../%1 -o main.exe -lbgi -lgdi32 -lcomdlg32 -luuid -loleaut32 -lole32
+g++ -fdiagnostics-color=always -g ../src/%1 -o main.exe -lbgi -lgdi32 -lcomdlg32 -luuid -loleaut32 -lole32
 
 if exist main.exe (
     echo Executable created in bin
